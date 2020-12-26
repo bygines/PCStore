1492787e9a61d146c678e6862373eaa1bb2fa278
Ginés López <sng_xtv@hotmail.com>
2020-12-26 22:04:15 +0100

    Login terminado,no hay opción de registrarse, por optimizar, nueva xhtml creado al que se redirecciona al loguearse,la cual es la pagina principal de la tienda de componentes pc. Falta pagina de resumen de compra y descarga en pdf de este.

---
+++ src/java/login/log.java
@@ -0,0 +1,38 @@
+package login;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Named;
+
+@Named(value = "log")
+@ApplicationScoped
+public class log {
+    private String user,pwd,language;
+    
+    public log() {
+    }
+    
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getPwd() {
+        return pwd;
+    }
+
+    public void setPwd(String pwd) {
+        this.pwd = pwd;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(String country) {
+        this.language = language;
+    }
+    
+}
